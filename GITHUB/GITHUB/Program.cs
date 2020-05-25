55b0cea05345396bec5f4845912c75c9e5694293
maciejonichimowski <65609072+maciejonichimowski@users.noreply.github.com>
2020-05-25 21:20:21 +0200

    123

---
+++ GITHUB/GITHUB/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GITHUB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 4;
+            int pole = a * b;
+            Console.WriteLine("Pole prostokata jest rowne: " +pole);
+            Console.ReadKey();
+          
+        }
+    }
+}
