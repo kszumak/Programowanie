5e59cf5cab01b760fdab2fab26ec00815ee048bc
Opi9038 <Opi9038@wp.pl>
2020-05-25 21:30:34 +0200

    321

--- GITHUB/GITHUB/Program.cs
+++ GITHUB/GITHUB/Program.cs
@@ -10,8 +10,13 @@ namespace GITHUB
             int b = 4;
             int pole = a * b;
             Console.WriteLine("Pole prostokata jest rowne: " +pole);
+            int A = 8;
+            int H = 12;
+            int POLE = A * H / 2;
+            Console.WriteLine("pole trójkąta jest równe: " + POLE);
             Console.ReadKey();
-          
+
+
         }
     }
 }
